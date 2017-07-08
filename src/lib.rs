11b24aefc5a016b46469cc33b0685ce20c65a8a1
KeyboardFire <andy@keyboardfire.com>
2017-07-08 14:26:45 -0500

    Implement Mtprs#send_enc and Mtprs#recv_enc
    For sending and receiving encoded messages.
    
    These are untested as of now.

--- src/lib.rs
+++ src/lib.rs
@@ -46,12 +46,15 @@ pub struct Mtprs {
     seq_num: u32,
     server_seq_num: u32,
     rsa: rsa::Rsa,
-    auth_key: Option<[u8; 256]>
+    auth_key: Option<[u8; 256]>,
+    auth_key_id: Option<[u8; 8]>,
+    server_salt: [u8; 8],
+    session_id: [u8; 8]
 }
 
 impl Mtprs {
     pub fn new() -> Mtprs {
-        Mtprs {
+        let mut m = Mtprs {
             stream: TcpStream::connect("149.154.167.40:443").unwrap(),
             seq_num: 0,
             server_seq_num: 0,
@@ -64,8 +67,13 @@ X4EQSjBbbdJ2ZXIsRrYOXfaA+xayEGB+8hdlLmAjbCVfaigxX0CDqWeR1yFL9kwd
 9P0NsZRPsmoqVwMbMu7mStFai6aIhc3nSlv8kg9qv1m6XHVQY3PnEw+QQtqSIXkl
 HwIDAQAB
 -----END PUBLIC KEY-----").unwrap(),
-            auth_key: None
-        }
+            auth_key: None,
+            auth_key_id: None,
+            server_salt: [0; 8],
+            session_id: [0; 8]
+        };
+        rand::thread_rng().fill_bytes(&mut m.session_id);
+        m
     }
 
     pub fn auth(&mut self) -> io::Result<()> {
@@ -78,6 +86,10 @@ HwIDAQAB
             let mut f = File::create("auth.key")?;
             f.write_all(&self.auth_key.unwrap())?;
         }
+        let mut key_id_arr = [0; 8];
+        let sha1 = hash::hash2(hash::MessageDigest::sha1(), &self.auth_key.unwrap()).unwrap();
+        key_id_arr.copy_from_slice(&sha1[12..20]);
+        self.auth_key_id = Some(key_id_arr);
         Ok(())
     }
 
@@ -239,6 +251,10 @@ HwIDAQAB
             panic!();  // TODO do something better
         }
 
+        for i in 0..8 {
+            self.server_salt[i] = new_nonce[i] ^ server_nonce[i];
+        }
+
         Ok(())
     }
 
@@ -272,6 +288,69 @@ HwIDAQAB
 
         println!("PRE: {:?}", buf);
         println!("SENDING DATA: {:?}", bytes);
+        self.stream.write(&buf)?;
+
+        Ok(())
+    }
+
+    fn send_enc(&mut self, bytes: &[u8]) -> io::Result<()> {
+        let seq_num = self.seq_num;
+        let raw_seq_num: [u8; 4] = enc(seq_num);
+        self.seq_num += 1;
+
+        let now = time::get_time();
+        let id = ((now.sec as f64 + now.nsec as f64 / 10f64.powi(9))
+                  * 2f64.powi(32)) as u64;
+        let raw_id: [u8; 8] = enc64(id);
+
+        let len = bytes.len() as u32;
+        let raw_len: [u8; 4] = enc(len);
+
+        let padding_len = if bytes.len() % 16 == 0 { 0 } else { 16 - bytes.len() % 16 };
+        let mut data: Vec<u8> = Vec::with_capacity(32 + bytes.len() + padding_len);
+        data.write(&self.server_salt)?;  // 8 bytes
+        data.write(&self.session_id)?;   // 8 bytes
+        data.write(&raw_id)?;            // 8 bytes
+        data.write(&raw_seq_num)?;       // 4 bytes
+        data.write(&raw_len)?;           // 4 bytes
+        data.write(&bytes)?;
+
+        let sha1 = hash::hash2(hash::MessageDigest::sha1(), &data).unwrap();
+        let (mut plain_a, mut plain_b, mut plain_c, mut plain_d) =
+            ([0; 48], [0; 48], [0; 48], [0; 48]);
+        plain_a[0..16].copy_from_slice(&sha1[4..20]);
+        plain_a[16..48].copy_from_slice(&self.auth_key.unwrap()[0..32]);
+        plain_b[0..16].copy_from_slice(&self.auth_key.unwrap()[32..48]);
+        plain_b[16..32].copy_from_slice(&sha1[4..20]);
+        plain_b[32..48].copy_from_slice(&self.auth_key.unwrap()[48..64]);
+        plain_c[0..32].copy_from_slice(&self.auth_key.unwrap()[64..96]);
+        plain_c[32..48].copy_from_slice(&sha1[4..20]);
+        plain_d[0..16].copy_from_slice(&sha1[4..20]);
+        plain_d[16..48].copy_from_slice(&self.auth_key.unwrap()[96..128]);
+        let (sha1_a, sha1_b, sha1_c, sha1_d) =
+            (hash::hash2(hash::MessageDigest::sha1(), &plain_a).unwrap(),
+             hash::hash2(hash::MessageDigest::sha1(), &plain_b).unwrap(),
+             hash::hash2(hash::MessageDigest::sha1(), &plain_c).unwrap(),
+             hash::hash2(hash::MessageDigest::sha1(), &plain_d).unwrap());
+        let (mut key, mut iv) = ([0; 32], [0; 32]);
+        key[0..8].copy_from_slice(&sha1_a[0..8]);
+        key[8..20].copy_from_slice(&sha1_b[8..20]);
+        key[20..32].copy_from_slice(&sha1_c[4..16]);
+        iv[0..12].copy_from_slice(&sha1_a[8..20]);
+        iv[12..20].copy_from_slice(&sha1_b[0..8]);
+        iv[20..24].copy_from_slice(&sha1_c[16..20]);
+        iv[24..32].copy_from_slice(&sha1_d[0..8]);
+
+        let mut padding = vec![0; padding_len];
+        rand::thread_rng().fill_bytes(&mut padding);
+        data.write(&padding)?;
+
+        let mut buf: Vec<u8> = vec![0; 24 + data.len()];;
+        buf.write(&self.auth_key_id.unwrap())?;
+        buf.write(&sha1[4..20])?;
+        let enc_key = aes::AesKey::new_encrypt(&key).unwrap();
+        aes::aes_ige(&data, &mut buf[24..24 + data.len()], &enc_key, &mut iv, symm::Mode::Encrypt);
+
         self.stream.write(&buf)?;
 
         Ok(())
@@ -315,6 +394,76 @@ HwIDAQAB
         println!("PRE: {:?}", data);
         data.drain(..20);
         println!("RECIEVING DATA: {:?}", data);
+        Ok(data)
+    }
+
+    fn recv_enc(&mut self) -> io::Result<Vec<u8>> {
+        let mut raw_packet_len = [0; 4];
+        self.stream.read(&mut raw_packet_len)?;
+        let packet_len: u32 = dec(raw_packet_len);
+
+        let mut raw_seq_num = [0; 4];
+        self.stream.read(&mut raw_seq_num)?;
+        let seq_num: u32 = dec(raw_seq_num);
+        if seq_num != self.server_seq_num {
+            println!("!!! BAD SEQUENCE NUMBER !!!");
+            println!("{} (expected) != {} (received)",
+                self.server_seq_num, seq_num);
+        }
+        self.server_seq_num += 1;
+
+        let mut buf = Vec::with_capacity((packet_len - 8) as usize);
+        <TcpStream as Read>::by_ref(&mut self.stream)
+            .take((packet_len - 8) as u64).read_to_end(&mut buf)?;
+
+        let mut raw_checksum = [0; 4];
+        self.stream.read(&mut raw_checksum)?;
+        let checksum = dec(raw_checksum);
+
+        let mut digest = crc32::Digest::new(crc32::IEEE);
+        digest.write(&raw_packet_len);
+        digest.write(&raw_seq_num);
+        digest.write(&buf);
+        let computed_checksum = digest.sum32();
+
+        if checksum != computed_checksum {
+            println!("!!! CHECKSUM MISMATCH !!!");
+            println!("{} (received) != {} (computed)",
+                checksum, computed_checksum);
+        }
+
+        buf.drain(..8);  // TODO maybe check the auth key id? heh
+        let msg_key: Vec<u8> = buf.drain(..16).collect();
+        let (mut plain_a, mut plain_b, mut plain_c, mut plain_d) =
+            ([0; 48], [0; 48], [0; 48], [0; 48]);
+        plain_a[0..16].copy_from_slice(&msg_key);
+        plain_a[16..48].copy_from_slice(&self.auth_key.unwrap()[8..40]);
+        plain_b[0..16].copy_from_slice(&self.auth_key.unwrap()[40..56]);
+        plain_b[16..32].copy_from_slice(&msg_key);
+        plain_b[32..48].copy_from_slice(&self.auth_key.unwrap()[56..72]);
+        plain_c[0..32].copy_from_slice(&self.auth_key.unwrap()[72..104]);
+        plain_c[32..48].copy_from_slice(&msg_key);
+        plain_d[0..16].copy_from_slice(&msg_key);
+        plain_d[16..48].copy_from_slice(&self.auth_key.unwrap()[104..136]);
+        let (sha1_a, sha1_b, sha1_c, sha1_d) =
+            (hash::hash2(hash::MessageDigest::sha1(), &plain_a).unwrap(),
+             hash::hash2(hash::MessageDigest::sha1(), &plain_b).unwrap(),
+             hash::hash2(hash::MessageDigest::sha1(), &plain_c).unwrap(),
+             hash::hash2(hash::MessageDigest::sha1(), &plain_d).unwrap());
+        let (mut key, mut iv) = ([0; 32], [0; 32]);
+        key[0..8].copy_from_slice(&sha1_a[0..8]);
+        key[8..20].copy_from_slice(&sha1_b[8..20]);
+        key[20..32].copy_from_slice(&sha1_c[4..16]);
+        iv[0..12].copy_from_slice(&sha1_a[8..20]);
+        iv[12..20].copy_from_slice(&sha1_b[0..8]);
+        iv[20..24].copy_from_slice(&sha1_c[16..20]);
+        iv[24..32].copy_from_slice(&sha1_d[0..8]);
+
+        let dec_key = aes::AesKey::new_decrypt(&key).unwrap();
+        let mut data = vec![0; buf.len()];
+        aes::aes_ige(&buf, &mut data, &dec_key, &mut iv, symm::Mode::Decrypt);
+
+        println!("decrypted: {:?}", data);
         Ok(data)
     }
 }
